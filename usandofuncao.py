18ddc04d2c24b74554a473410bc32cbcc6106374
Your Name <josevictormobile@gmail.com>
2022-09-29 00:21:52 -0300

    aprendendo a usar função, if, elif e else

---
+++ usandofuncao.py
@@ -0,0 +1,10 @@
+def comparar(num1, num2):
+    if num1 > num2:
+        print(f"O {num1} é maior que o {num2}")
+    elif num1 == num2:
+        print("Os dois numeros sao iguais")
+    else:
+        print(f"O {num2} é maior que o {num1}")
+
+
+comparar(5, 7)
